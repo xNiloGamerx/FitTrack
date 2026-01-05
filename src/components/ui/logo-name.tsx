ab14aa6ede2708c65d6d9ebd939bf67e920e723f
xNiloGamerx <nikb0322@gmail.com>
2026-01-05 23:03:25 +0100

    Added gitignore, Updated Navbar logo padding

--- src/components/ui/logo-name.tsx
+++ src/components/ui/logo-name.tsx
@@ -2,14 +2,14 @@ import { Flame } from "lucide-react";
 import Link from "next/link";
 
 export default function LogoName() {
-  return (
-    <Link href={"/"}>
-      <div className="flex items-center gap-3">
-        <div className="bg-primary p-2 rounded-lg">
-          <Flame className="w-6 h-6 text-primary-foreground" />
-        </div>
-        <h1 className="text-xl font-bold">FitTrack</h1>
-      </div>
-    </Link>
-  );
+	return (
+		<Link href={"/"}>
+			<div className="flex items-center gap-3 p-2">
+				<div className="bg-primary p-2 rounded-lg">
+					<Flame className="w-6 h-6 text-primary-foreground" />
+				</div>
+				<h1 className="text-xl font-bold">FitTrack</h1>
+			</div>
+		</Link>
+	);
 }

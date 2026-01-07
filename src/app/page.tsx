08d8e2effb99ee4b32143b65750a055caa7ffd92
xNiloGamerx <nikb0322@gmail.com>
2026-01-07 20:11:27 +0100

    Updated Water Card to be mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, CircleArrowRight, Droplet } from "lucide-react";
+import { ArrowRight, Droplet } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
         Dashboard
       </h1>
       <div className="p-4 w-full">
-        <div className="flex flex-col items-center justify-start gap-3 max-w-80 bg-card rounded-2xl">
+        <div className="flex flex-col items-center justify-start gap-3 md:max-w-90 bg-card rounded-2xl">
           {/* w-60 h-60 */}
           <div className="flex items-center justify-center gap-1 w-full pt-3 pb-2 border-b-2 border-sidebar-border">
             <Droplet className="fill-[#00beff] text-[#00beff]" />
@@ -21,7 +21,8 @@ export default function Home() {
             <div className="w-9 h-5 bg-[#5f8794] rounded-t-lg rounded-b-xs"></div>
             <div className="w-5 h-2 bg-[rgba(179,236,255,0.9)]"></div>
             <div className="bg-[rgba(179,236,255,0.9)] w-15 h-25 rounded-lg"></div>
-            <div className="absolute bg-[#00bfff94] w-12 h-22 bottom-4.5 rounded-lg"></div>
+            <div className="absolute bg-[#00bfff94] w-12 h-15 bottom-4.5 rounded-lg"></div>
+            {/* Maximale Wasser Höhe: h-22 */}
           </div>
           <div className="flex flex-col items-center justify-start gap-2 w-full px-5 pb-5">
             <div className="flex items-center justify-center gap-2 w-full">
@@ -43,7 +44,7 @@ export default function Home() {
                 <button className="cursor-pointer" type="button">
                   Water Tracking
                 </button>
-                <ArrowRight className="w-5 h-5 transition-transform group-hover:translate-x-1" />
+                <ArrowRight className="w-5 h-5 transition-transform group-hover:translate-x-1 group-active:translate-x-1" />
               </div>
             </Link>
           </div>
